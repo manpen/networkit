5dcbe8001ecdb9925f9bafe095e4efdbfa054837
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2019-03-08 18:03:47 +0100

    Add iterations

--- benchmark.py
+++ benchmark.py
@@ -20,6 +20,7 @@ def power10series(begin, end, steps_per_dec):
         i += 1
 
 TimeoutSeconds = 100
+Iterations = 5
 Ts = [0.0, 0.5, 2.0, 5.0, 10.0]
 Alphas = [2.2, 2.5, 3.0]
 Ns = list(power10series(1e3, 1e7, 3))
@@ -31,11 +32,11 @@ with open('nkgen_bench.csv', 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fields)
     writer.writeheader()
 
-    for T, alpha, deg in itertools.product(Ts, Alphas, AvgDegs):
+    for iter, T, alpha, deg in itertools.product(range(Iterations), Ts, Alphas, AvgDegs):
         time_ms = 0
 
         for n in Ns:
-            print("T: %.1f alpha: %.1f n: % 8d deg: % 4d" % (T, alpha, n, deg))
+            print("Iter: % 2d T: %.1f alpha: %.1f n: % 8d deg: % 4d" % (iter, T, alpha, n, deg))
 
             if (time_ms >= 0):
                 try:
