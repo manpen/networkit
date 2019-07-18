5d15401939cd0141d82ed4e9d5ef48d3ee006010
Manuel Penschuck <networkit@manuel.jetzt>
2019-07-18 14:20:38 +0200

    Move computeCommonDisjointNeighbour into Helper header

--- networkit/cpp/randomization/curveball/CurveballImpl.hpp
+++ networkit/cpp/randomization/curveball/CurveballImpl.hpp
@@ -21,6 +21,7 @@
 
 #include "AdjacencyList.hpp"
 #include "TradeList.hpp"
+#include "Helper.hpp"
 
 namespace NetworKit {
 namespace CurveballDetails {
@@ -47,13 +48,13 @@ public:
         assert(numNodes > 0);
     }
 
+    template <bool Directed = false>
 	void run(const trade_vector &trades) {
         if (!hasRun)
             loadFromGraph(trades);
         else
             restructureGraph(trades);
 
-        count trade_count = 0;
         neighbour_vector common_neighbours;
         neighbour_vector disjoint_neighbours;
 
@@ -64,122 +65,76 @@ public:
 
         auto &urng = Aux::Random::getURNG();
 
-        for (const auto &trade : trades) {
+        constexpr bool allowSelfLoops = false;
+
+        for (const auto trade : trades) {
             handler.assureRunning();
 
             // Trade partners u and v
-            const node u = trade.first;
-            const node v = trade.second;
+            const node u = std::min(trade.first, trade.second);
+            const node v = std::max(trade.first, trade.second);
 
             numAffectedEdges += adjList.degreeAt(u);
             numAffectedEdges += adjList.degreeAt(v);
 
-            // Shift the tradeList offset for these two, currently was set to
-            // trade_count
+            // Shift the tradeList offset for these two, currently was set to trade_count
             tradeList.incrementOffset(u);
             tradeList.incrementOffset(v);
 
-            // Retrieve respective neighbours
-            // we return whether u has v in his neighbors or vice-versa
-            auto organize_neighbors = [&](node node_x, node node_y) {
-                auto pos = std::find(adjList.begin(node_x), adjList.end(node_x), node_y);
-                if (pos == adjList.cend(node_x)) {
-                    // element not found, sort anyway
-                    std::sort(adjList.begin(node_x), adjList.end(node_x));
+            const auto u_begin = adjList.begin(u);
+            const auto v_begin = adjList.begin(v);
 
-                    return false;
-                } else {
-                    // overwrite node_y's position with END
-                    *pos = LISTROW_END;
+            auto u_end = adjList.end(u);
+            auto v_end = adjList.end(v);
 
-                    // sort, such that node_y's position is at end - 1
-                    std::sort(adjList.begin(node_x), adjList.end(node_x));
-
-                    // overwrite with node_y again
-                    *(adjList.end(node_x) - 1) = node_y;
-
-                    return true;
+            // Check whether there exist edges of form (u, v) or (v, u).
+            // If this is the case, they are removed from the neighbourhood
+            bool edge_between_uv = false;
+            bool edge_between_vu = false;
+            if (!Directed || !allowSelfLoops) {
+                auto it = std::find(u_begin, u_end, v);
+                if (it != u_end) {
+                    *it = *(--u_end);
+                    edge_between_uv = true;
                 }
-            };
 
-            const bool u_share = organize_neighbors(u, v);
-            const bool v_share = organize_neighbors(v, u);
-            auto u_end = (u_share ? adjList.cend(u) - 1 : adjList.cend(u));
-            auto v_end = (v_share ? adjList.cend(v) - 1 : adjList.cend(v));
-
-            const bool shared = u_share || v_share;
-
-            // both can't have each other, only inserted in one
-            assert((!u_share && !v_share) || (u_share != v_share));
-
-            // No need to keep track of direct positions
-            // Get common and disjoint neighbors
-            // Here sort and parallel scan
-            common_neighbours.clear();
-            disjoint_neighbours.clear();
-            auto u_nit = adjList.cbegin(u);
-            auto v_nit = adjList.cbegin(v);
-            while ((u_nit != u_end) && (v_nit != v_end)) {
-                assert(*u_nit != v);
-                assert(*v_nit != u);
-                if (*u_nit > *v_nit) {
-                    disjoint_neighbours.push_back(*v_nit);
-                    v_nit++;
-                    continue;
-                }
-                if (*u_nit < *v_nit) {
-                    disjoint_neighbours.push_back(*u_nit);
-                    u_nit++;
-                    continue;
-                }
-                // *u_nit == *v_nit
-                {
-                    common_neighbours.push_back(*u_nit);
-                    u_nit++;
-                    v_nit++;
+                if (Directed) {
+                    auto it = std::find(v_begin, v_end, u);
+                    if (it != v_end) {
+                        *it = *(--v_end);
+                        edge_between_vu = true;
+                    }
                 }
             }
-            if (u_nit == u_end)
-                disjoint_neighbours.insert(disjoint_neighbours.end(), v_nit, v_end);
-            else
-                disjoint_neighbours.insert(disjoint_neighbours.end(), u_nit, u_end);
 
-            const count u_setsize = static_cast<count>(u_end - adjList.cbegin(u) -
-                                                       common_neighbours.size());
-            const count v_setsize = static_cast<count>(v_end - adjList.cbegin(v) -
-                                                       common_neighbours.size());
-            // v_setsize not necessarily needed
+            // Compute common / disjoint neighbors
+            CurveballDetails::computeCommonDisjointNeighbour(u_begin, u_end, v_begin, v_end, common_neighbours, disjoint_neighbours);
 
             // Reset fst/snd row
             adjList.resetRow(u);
             adjList.resetRow(v);
 
-            tlx::random_bipartition_shuffle(disjoint_neighbours.begin(),
-                                            disjoint_neighbours.end(), u_setsize, urng);
 
-            // Assign first u_setsize to u and last v_setsize to v
-            // if not existent then max value, and below compare goes in favor of
-            // partner, if partner has no more neighbours as well then their values are
-            // equal (max and equal) and tiebreaking is applied
-            for (count counter = 0; counter < u_setsize; counter++) {
-                const node swapped = disjoint_neighbours[counter];
-                update(u, swapped);
-            }
-            for (count counter = u_setsize; counter < u_setsize + v_setsize;
-                 counter++) {
-                const node swapped = disjoint_neighbours[counter];
-                update(v, swapped);
-            }
-            // Distribute common edges
+            // Shuffle nodes; [begin, splitter) belong to u, [splitter, end) belong to v
+            const auto u_setsize = std::distance(u_begin, u_end) - common_neighbours.size();
+            const auto splitter = disjoint_neighbours.cbegin() + u_setsize;
+            tlx::random_bipartition_shuffle(disjoint_neighbours.begin(), disjoint_neighbours.end(), u_setsize, urng);
+
+            // Send u's neighbours
+            if (edge_between_uv) update(u, v);
+            for(auto it = disjoint_neighbours.cbegin(); it != splitter; ++it)
+                update(u, *it);
+
+            // Send v's neighbours
+            if (edge_between_vu) update(v, u);
+            for(auto it = splitter; it != disjoint_neighbours.cend(); ++it)
+                update(v, *it);
+
+            // Send common neighbours
             for (const auto common : common_neighbours) {
                 update(u, common);
                 update(v, common);
             }
-            // Do not forget edge between u and v
-            if (shared)
-                update(u, v);
-
-            trade_count++;
         }
 
         hasRun = true;
@@ -236,7 +191,7 @@ protected:
 		const tradeid ta = *(tradeList.getTrades(a));
 		const tradeid tb = *(tradeList.getTrades(b));
 
-		if (ta <= tb) {
+		if (std::tie(ta, a) <= std::tie(tb, b)) {
 			adjList.insertNeighbour(a, b);
 		} else {
 			adjList.insertNeighbour(b, a);
