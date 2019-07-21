b091a29678a67c3df14a59f40afd6dbc05606ecb
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2019-07-21 11:07:55 +0200

    Smaller steps in Benchmark

--- run-benchmark.py
+++ run-benchmark.py
@@ -15,32 +15,44 @@ def loadGraph(filename):
     nRight = G.numberOfNodes() - nLeft
     nLeft, nRight
 
-    return G.toUndirected(), nLeft, nRight
+    return G, nLeft, nRight
 
 def benchmarkCurveball(G, nTrades, nodeLower, nodeUpper):
     """Returns runtime in seconds"""
-    alg = nk.randomization.Curveball(G)
-    trades = nk.randomization.CurveballUniformTradeGenerator(nTrades, (nodeLower, nodeUpper))
+    alg = nk.randomization.Curveball(G, False, True)
+
+    iters = 1000
+
+    trades = nk.randomization.CurveballUniformTradeGenerator(nTrades // iters, (nodeLower, nodeUpper))
 
     start = time.time()
-    alg.run(trades)
+
+    for i in range(iters):
+        alg.run(trades)
+        alg.getGraph()
 
     return 1000 * (time.time() - start)
 
 print("Loading graphs")
-datasets = [loadGraph(fn) for fn in glob("netflix100*.txt")]
+datasets = [loadGraph(fn) for fn in glob("netflix1000.txt")]
 
 with open("nk-curveball.csv", "w") as out:
-    out.write("iter,nNodes,nEdges,nLeft,nRight,nTrades,timeMS\n")
+    out.write("algo,iter,nNodes,nEdges,nLeft,nRight,nTrades,timeMS\n")
     for iter in range(30):
         for (G, nLeft, nRight) in datasets:
-            nTrades = 10 * nRight
-            print("Run with nRight = %d ..." % nRight)
-            duration = benchmarkCurveball(G, nTrades, nLeft, nLeft + nRight)
-            print(" ... took %f ms" % duration)
+            for directed in [True, False]:
+                nTrades = 10 * nRight
+                print("Run with nRight = %d ..." % nRight)
+                if directed:
+                    duration = benchmarkCurveball(G, nTrades, nLeft, nLeft + nRight)
+                else:
+                    duration = benchmarkCurveball(G.toUndirected(), nTrades, nLeft, nLeft + nRight)
 
-            out.write("%d,%d,%d,%d,%d,%d,%f" % (
-                iter, G.numberOfNodes(), G.numberOfEdges(),
-                nLeft, nRight, nTrades, duration
-            ))
-            out.flush()
+                print(" ... took %f ms" % duration)
+
+                out.write("nkcb-%s,%d,%d,%d,%d,%d,%d,%f\n" % (
+                    "direct" if directed else "undirect",
+                    iter, G.numberOfNodes(), G.numberOfEdges(),
+                    nLeft, nRight, nTrades, duration
+                ))
+                out.flush()
