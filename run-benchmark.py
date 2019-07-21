ead593c794e439b37ab2cc89b7ca65a7e57a202f
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2019-07-21 14:03:51 +0200

    Benchmark now include check for perturbation

--- run-benchmark.py
+++ run-benchmark.py
@@ -8,13 +8,10 @@ def loadGraph(filename):
     print(" loading ", filename)
     G = nk.graphio.NetworkitBinaryReader().read(filename)
 
-    nLeft = 0
-    while G.degreeOut(nLeft) > 0:
-        nLeft += 1
-    nRight = G.numberOfNodes() - nLeft
-    nLeft, nRight
+    nLeft = sum(int(G.degreeOut(i) > 0) for i in range(G.numberOfNodes()))
+    assert(0 < nLeft < G.numberOfNodes())
 
-    return G, nLeft, nRight
+    return G, nLeft
 
 def benchmarkCurveball(G, nTrades, nodeLower, nodeUpper):
     """Returns runtime in seconds"""
@@ -23,72 +20,59 @@ def benchmarkCurveball(G, nTrades, nodeLower, nodeUpper):
     trades = nk.randomization.CurveballUniformTradeGenerator(nTrades, (nodeLower, nodeUpper))
 
     start = time.time()
-
     alg.run(trades)
+    stop = time.time()
 
-    return 1000 * (time.time() - start)
-
-
-def computePerturbation(G, nTrades, nodeLower, nodeUpper):
-    alg = nk.randomization.Curveball(G, False, True)
-
-    iters = 10
-    steps = nTrades // iters
-
-    trades = nk.randomization.CurveballUniformTradeGenerator(steps, (nodeLower, nodeUpper))
-
-    start = time.time()
-
-    for i in range(iters):
-        alg.run(trades.generate())
-        G_rand = alg.getGraph()
-
-        assert(G_rand.numberOfNodes() == G.numberOfNodes())
-        assert(G_rand.numberOfEdges() == G.numberOfEdges())
-
-        pert = nk.distance.EdgeSetDistance(G, G_rand).run()
-        print(steps * (i+1), alg.getNumberOfAffectedEdges(), G.numberOfEdges(), pert.sizeOfIntersection(), pert.sizeOfUnion())
-
+    # Make sure some perturbation took place
+    Grand = alg.getGraph()
+    pert = nk.distance.EdgeSetDistance(G, Grand).run()
+    print(" ... perturbation score: ", pert.perturbationScore())
     assert(G.numberOfEdges() != pert.sizeOfIntersection())
 
-    return 1000 * (time.time() - start)
+    return 1000 * (stop - start)
 
 print("Loading graphs")
 datasets = []
 for fn in glob("netflix1000*.nkb"):
     start = time.time()
-    G, nLeft, nRight = loadGraph(fn)
+    G, nLeft = loadGraph(fn)
     mid = time.time()
     datasets.append((G, "l", 0, nLeft))
     print(" ... transpose")
-    datasets.append((G.transpose(), "r", nLeft, nLeft + nRight))
+    Gt = G.transpose()
+    datasets.append((Gt, "r", nLeft, G.numberOfNodes()))
     end = time.time()
     print(" ... took (%.1f ms and %.1f ms)" % (1000 * (mid - start), 1000 * (end - mid)))
 
+
 with open("nk-curveball.csv", "w") as out:
-    out.write("algo,iter,nNodes,nEdges,activeSide,nActive,nTrades,timeMS\n")
+    out.write("algo,iter,nNodes,nEdges,activeSide,nActive,nTrades,nRight,timeMS\n")
 
     for iter in range(10):
         for (G, activeSide, lower, upper) in datasets:
             assert(G.isDirected())
+            assert(lower < upper)
 
-            for i in range(lower, upper):
-                assert(G.degreeIn(i) == 0)
+            # make [upper, lower) contains all nodes with out-edges
+            assert(sum((G.degreeOut(i) for i in range(lower, upper))) == G.numberOfEdges())
+            assert(sum((G.degreeIn(i) for i in range(lower, upper))) == 0)
+
+            nRight = G.numberOfNodes() - (lower if lower > 0 else upper)
 
             for directed in [True, False]:
                 nActive = upper - lower
-                nTrades = 10 * nActive
-                print("Run with nRight = %d ..." % nRight)
+                nTrades = 100 # * nActive
+                print("Run on [%d, %d) with nRight = %d, directed = %d, side = %s ..." % (lower, upper, nRight, directed, activeSide))
                 if directed:
-                    duration = benchmarkCurveball(G, nTrades, 0, nLeft)
+                    duration = benchmarkCurveball(G, nTrades, lower, upper)
                 else:
                     duration = benchmarkCurveball(G.toUndirected(), nTrades, lower, upper)
 
                 print(" ... took %f ms" % duration)
 
-                out.write("nkcb-%s,%d,%d,%d,%s,%d,%d,%f\n" % (
+                out.write("nkcb-%s,%d,%d,%d,%s,%d,%d,%d,%f\n" % (
                     "direct" if directed else "undirect",
                     iter, G.numberOfNodes(), G.numberOfEdges(),
-                    activeSide, nActive, nTrades, duration
+                    activeSide, nActive, nTrades, nRight, duration
                 ))
                 out.flush()
